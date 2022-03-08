d3d9571ca6a2bf7e9fb90d70bc76daa50dadcc1e
IOWEB TECHNOLOGIES <info@ioweb.gr>
2022-03-08 18:32:13 +0200

    add group extension model

---
+++ privateTemplates/crudDbSchema/Model/${Entityname}Extension.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Copyright (c) 2022. IOWEB TECHNOLOGIES
+ */
+
+namespace ${Vendorname}\${Modulename}\Model;
+
+
+use ${Vendorname}\${Modulename}\Api\Data\GroupExtensionInterface;
+
+class ${Entityname}Extension extends \Magento\Framework\Api\AbstractSimpleObject implements ${Entityname}ExtensionInterface
+{
+
+}
