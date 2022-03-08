30fbb24c6ea06db809ae28fba204c17d7beb0ddc
IOWEB TECHNOLOGIES <info@ioweb.gr>
2022-03-08 16:13:08 +0200

    fix interface namespace

--- privateTemplates/crudDbSchema/Model/${Entityname}.php
+++ privateTemplates/crudDbSchema/Model/${Entityname}.php
@@ -12,7 +12,7 @@ namespace ${Vendorname}\${Modulename}\Model;
 use Ioweb\StockManager\Api\Data\StockEntryExtensionInterface;use Magento\Framework\DataObject\IdentityInterface;
 use Magento\Framework\Model\AbstractExtensibleModel;
 use Magento\Framework\Model\AbstractModel;
-use ${Vendorname}\${Modulename}\Api\Data
+use ${Vendorname}\${Modulename}\Api\Data\${Entityname}Interface;
 
 class ${Entityname} extends AbstractExtensibleModel implements IdentityInterface, ${Entityname}Interface
 {
